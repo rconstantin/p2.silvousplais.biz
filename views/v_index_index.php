2e10631b2a6129ac0bb6bd2d75cf8a366a02ac14
Robbie Constantin <robbieconstantin@gmail.com>
2013-10-30 11:38:09 -0400

    html5 validation

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -5,13 +5,12 @@
 </h1>
 
 <img class="floatleft" src="/uploads/avatars/busytown2.jpg" alt="" width="200" height="150">
-<p1><?=APP_NAME?> is a micro blog where people can join to share messages with other members. First time visitors, need to signup. The email/password fields will be used to login in subsequent visits.</p1> <br><br>
+<p><?=APP_NAME?> is a micro blog where people can join to share messages with other members. First time visitors, need to signup. The email/password fields will be used to login in subsequent visits.</p> 
     
-<p1>Once signed up/logged in, members can modify their profiles, manage their connections with other members (follow/unfollow), view a list of their followers, modify or delete their old posts and write new posts. </p1>
-
-<br> <br> <br>
-
-<p1> +1 Feature: successful Signup will redirect new member to Profile Page</p1> <br>
-<p1> +1 Feature: upload URL Avatar in Profile page and display Blog statistics</p1> <br>
-<p1> +1 Feature: Update/Delete Blog Posts </p1><br>
+<p>Once signed up/logged in, members can modify their profiles, manage their connections with other members (follow/unfollow), view a list of their followers, modify or delete their old posts and write new posts. </p>
+<br>
+<p> +1 Feature: successful Signup will redirect new member to Profile Page.<br>
++1 Feature: upload URL Avatar in Profile page and display Blog statistics.<br> 
+ +1 Feature: Update/Delete Blog Posts.
+</p>
 
