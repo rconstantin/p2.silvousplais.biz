907a0a57a9239df1e2e0d1d97487da075088e9a0
Robbie Constantin <robbieconstantin@gmail.com>
2013-11-02 09:03:16 -0400

    fixed indentation

--- views/_v_template.php
+++ views/_v_template.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php if(isset($title)) echo $title; ?></title>
+    <title><?php if(isset($title)) echo $title; ?></title>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
     <!-- Common JS/CSS -->
     <!-- Common CSS/JSS -->
     <link rel="stylesheet" href="/css/app.css" type="text/css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>					
-	<!-- Controller Specific JS/CSS -->
-	<?php if(isset($client_files_head)) echo $client_files_head; ?>
+    <!-- Controller Specific JS/CSS -->
+    <?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
 </head>
 
