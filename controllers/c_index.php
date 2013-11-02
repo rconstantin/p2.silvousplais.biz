78605006b5beec4221048fa848c1bc8c2982c5b7
Robbie Constantin <robbieconstantin@gmail.com>
2013-11-02 08:43:31 -0400

    fixed indentation, removed commented out code

--- controllers/c_index.php
+++ controllers/c_index.php
@@ -23,14 +23,7 @@ class index_controller extends base_controller {
         # add Menu if user already logged in (taking advantage of the global $this->user)
         $this->template->hide_menu = FALSE;
         $this->template->menu = View::instance('v_menu');
-		# CSS/JS includes
-			/*
-			$client_files_head = Array("");
-	    	$this->template->client_files_head = Utils::load_client_files($client_files);
-	    	
-	    	$client_files_body = Array("");
-	    	$this->template->client_files_body = Utils::load_client_files($client_files_body);   
-	    	*/
+		
 		# Render the view
 		echo $this->template;
 	 
