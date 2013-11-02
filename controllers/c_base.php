dc6cc6250244d15ba61e3eaeac2ecb1a2052c3ca
Robbie Constantin <robbieconstantin@gmail.com>
2013-11-02 08:54:11 -0400

    fixed indentation

--- controllers/c_base.php
+++ controllers/c_base.php
@@ -13,18 +13,19 @@ class base_controller {
 	public function __construct() {
 						
 		# Instantiate User obj
-			$this->userObj = new User();
+		$this->userObj = new User();
 			
 		# Authenticate / load user
-			$this->user = $this->userObj->authenticate();					
+		$this->user = $this->userObj->authenticate();					
 						
 		# Set up templates
-			$this->template 	  = View::instance('_v_template');
-			$this->email_template = View::instance('_v_email');			
+		$this->template 	  = View::instance('_v_template');
+		$this->email_template = View::instance('_v_email');			
 								
 		# So we can use $user in views			
-			$this->template->set_global('user', $this->user);
-			
+		$this->template->set_global('user', $this->user);
+
 	}
-	
+
 } # eoc
+?>
